f771ad8746037e092f2f8045d3d9190911afabbc
naegahyung <naegahyung@gmail.com>
2019-04-11 15:30:29 -0400

    adding main func

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,13 @@ from flask import Flask
 from routes import initialize_routes
 from dotenv import load_dotenv
 import json
+import os
 
 load_dotenv()
 app = Flask(__name__, static_folder=None)
-initialize_routes(app)
 
+if __name__ == "__main__":
+    initialize_routes(app)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
