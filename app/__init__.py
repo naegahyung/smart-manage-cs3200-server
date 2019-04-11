99132b36d0045e3999ce2f02aba0a6272a35f5f3
naegahyung <naegahyung@gmail.com>
2019-04-11 15:44:36 -0400

    port

--- app/__init__.py
+++ app/__init__.py
@@ -10,5 +10,5 @@ app = Flask(__name__, static_folder=None)
 if __name__ == "__main__":
     initialize_routes(app)
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(port=port)
 
